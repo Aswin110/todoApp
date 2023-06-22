c66d0410b3d6ceeb2c132e1cc35f05026267926f
Aswin110 <aswinashok110@gmail.com>
2023-06-22 08:00:08 +0530

    create a todoApp
    create a todoApp using vue.js

---
+++ src/components/todoApp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">My Vue Todo App</h2> 
+    <!--input-->
+    <div class="d-flex">
+      <input type="text" placeholder="enter task " class="form-control">
+      <button class="btn btn-warning rounded-0">SUBMIT</button>
+    </div>
+
+    <!--task table-->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">task</th>
+          <th scope="col">status</th>
+          <th scope="col" class="text-centre">#</th>
+          <th scope="col" class="text-centre">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>watch kayal</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
